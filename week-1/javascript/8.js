bd1ffe25052ee71e991cbac09219420fdeb607b9
Nelson <quimconelson@gmail.com>
2024-05-30 14:16:22 +0800

    [8.js]function that returns all vowels in a string

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -3,3 +3,19 @@
  * @param {string} str The string to check.
  * @return {string} The string with all vowels removed.
  */
+function getVowels(str) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var result = "";
+
+  for (var char of str) {
+    if (!vowels.includes(char.toLowerCase())) {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+var str = "AEioTu1";
+var result = getVowels(str);
+console.log(result);
