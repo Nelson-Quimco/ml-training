947fbf2833fcb4663f7bfceff1df568edda1a4ce
Nelson <quimconelson@gmail.com>
2024-05-30 09:42:55 +0800

    [2.js]calculate all the numbers in the arry and return the calculated sum.

--- week-1/javascript/2.js
+++ week-1/javascript/2.js
@@ -3,3 +3,14 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+function calSum(arr) {
+   var sum = 0;
+   for (var num of arr) {
+     sum += num;
+   }
+   return sum;
+  //return arr.reduce((a,b)=>{return b+a });
+ }
+ var arr = [1, 2, 3, 4, 5,6];
+console.log(calSum(arr)); 
