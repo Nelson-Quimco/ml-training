25ce532e480e1552048b4f807e3bc827952f882c
Nelson <quimconelson@gmail.com>
2024-05-30 13:51:07 +0800

    [js.12] function that takes an array of numbers and throws a custom 'Error' if the array is empty

--- week-1/javascript/12.js
+++ week-1/javascript/12.js
@@ -3,3 +3,15 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+function checkArray(arr) {
+  if (arr.length === 0) {
+    throw new Error("The array is empty.");
+  }
+}
+try {
+  checkArray([1, 2, 3]);
+  console.log("The array is not empty.");
+} catch (error) {
+  console.log(error.message);
+}
