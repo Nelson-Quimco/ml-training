eddc7683641b8f7106f17fde995b05acacdecf8a
Nelson <quimconelson@gmail.com>
2024-05-30 11:40:58 +0800

    [6.js] converts Fahrenheit to Celsius

--- week-1/javascript/6.js
+++ week-1/javascript/6.js
@@ -4,3 +4,13 @@
  * @param {number} fahrenheit The temperature in Fahrenheit.
  * @return {number} The temperature in Celsius.
  */
+
+
+var fahrenheit = 140;
+var celsius = fahrenheitToCelsius(fahrenheit);
+console.log(fahrenheit + "°F = " + celsius + "°C");
+
+function fahrenheitToCelsius(fahrenheit) {
+    var celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+  }
