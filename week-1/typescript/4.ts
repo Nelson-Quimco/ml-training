d85234100e493786135b90d7fd0c7198631fd7af
Nelson <quimconelson@gmail.com>
2024-06-01 11:26:55 +0800

    [4.ts]function to display the current date and time in the format

--- week-1/typescript/4.ts
+++ week-1/typescript/4.ts
@@ -3,3 +3,19 @@
  * 'MM-DD-YYYY HH:MM:SS'.
  * @return {string} The current date and time in the format 'MM-DD-YYYY HH:MM:SS'.
  */
+function getCurrtDateTime(): string {
+  var now = new Date();
+
+  var month = String(now.getMonth() + 1).padStart(2, "0");
+  var day = String(now.getDate()).padStart(2, "0");
+  var year = now.getFullYear();
+
+  var hours = String(now.getHours()).padStart(2, "0");
+  var minutes = String(now.getMinutes()).padStart(2, "0");
+  var seconds = String(now.getSeconds()).padStart(2, "0");
+
+  var formattedDateTime = `${month}-${day}-${year} ${hours}:${minutes}:${seconds}`;
+
+  return formattedDateTime;
+}
+console.log(getCurrtDateTime());
