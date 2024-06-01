da692df064e848349a3f4261dd75d15829d89700
Nelson <quimconelson@gmail.com>
2024-06-01 13:13:58 +0800

    [12.ts]function that takes an array of numbers and throws a custom 'Error' if the array is empty.

--- week-1/typescript/12.ts
+++ week-1/typescript/12.ts
@@ -3,3 +3,15 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+function checkArray(arr: number[]) {
+  if (arr.length === 0) {
+    throw new Error("The array is empty.");
+  }
+}
+try {
+  checkArray([]);
+  console.log("The array is not empty.");
+} catch (error) {
+  console.log(error.message);
+}
