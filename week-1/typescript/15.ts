b2033007b4b421963bd34e9b60eaa26598a93256
Nelson <quimconelson@gmail.com>
2024-06-01 13:16:43 +0800

    [15.ts]function that checks and outputs whether the number is even or odd.

--- week-1/typescript/15.ts
+++ week-1/typescript/15.ts
@@ -2,3 +2,14 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+
+function checkEvenOrOdd(n: number) {
+  for (var i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " is even.");
+    } else {
+      console.log(i + " is odd.");
+    }
+  }
+}
+checkEvenOrOdd(7);
