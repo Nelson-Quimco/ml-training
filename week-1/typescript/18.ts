d8cc3100006ca14a975f0424ae76e5010a62f1bc
Nelson <quimconelson@gmail.com>
2024-06-01 13:35:23 +0800

    [18.ts] functions that will create a shopping cart that can add item in the cart, remove and display the total items and number of items in the cart

--- week-1/typescript/18.ts
+++ week-1/typescript/18.ts
@@ -16,3 +16,82 @@
  * @param {string[]} items
  * @returns {string}
  */
+
+const itemsList = [
+  {
+    name: "apple",
+    price: 10,
+  },
+  {
+    name: "orange",
+    price: 10,
+  },
+  {
+    name: "grapes",
+    price: 180,
+  },
+  {
+    name: "banana",
+    price: 7,
+  },
+  {
+    name: "melon",
+    price: 50,
+  },
+];
+
+let cart = [];
+
+function addItems(items: string): string {
+  if (cart.length < 10) {
+    cart.push(items);
+    return `Items: ${cart}`;
+  } else {
+    return "The cart is full.";
+  }
+}
+function discardItem(items: string) {
+  if (cart.includes(items)) {
+    const indx = cart.indexOf(items);
+    cart.splice(indx, 1);
+    return `${items} Successfully Discarded \nUpdated Items: ${cart}`;
+  } else if (!cart.length) {
+    return "Add item first";
+  } else {
+    return "Item Not Found";
+  }
+}
+
+function displayTotalPrice() {
+  let sum = 0;
+  for (const items of itemsList) {
+    for (const cartItem of cart) {
+      if (items.name == cartItem) {
+        sum += items.price;
+      }
+    }
+  }
+  return `Total: ${sum}`;
+}
+
+function displayCartQuantity() {
+  const size = cart.length;
+  return `Cart Quantity: ${size}`;
+}
+console.log(addItems("apple"));
+console.log(addItems("banana"));
+console.log(addItems("melon"));
+console.log(addItems("banana"));
+console.log(addItems("orange"));
+console.log(addItems("grapes"));
+console.log(addItems("apple"));
+console.log(addItems("melon"));
+console.log(addItems("banana"));
+console.log(addItems("orange"));
+
+console.log(addItems("grapes"));
+console.log(addItems("apple"));
+console.log(addItems("orange"));
+console.log(discardItem("banana"));
+console.log(displayTotalPrice());
+console.log(displayCartQuantity());
