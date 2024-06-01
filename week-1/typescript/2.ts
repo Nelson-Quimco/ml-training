fc5d1c45ec402ccf982be696d10ba78d425c8e4d
Nelson <quimconelson@gmail.com>
2024-06-01 11:06:54 +0800

    [2.ts] function to calculate the sum of all numbers in an array.

--- week-1/typescript/2.ts
+++ week-1/typescript/2.ts
@@ -3,3 +3,10 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+function calculateSum(arr: number[]) {
+  return arr.reduce((a, b) => {
+    return b + a;
+  });
+}
+var arr = [1, 2, 3, 4, 5, 6];
+console.log(calculateSum(arr));
