ca0ce27c32d77c728315645bac0fc208cc98cabc
Nelson <quimconelson@gmail.com>
2024-06-01 13:03:37 +0800

    [7.ts] function that takes a number as a parameter and throws a custom 'Error' if the number is not an integer.

--- week-1/typescript/7.ts
+++ week-1/typescript/7.ts
@@ -3,3 +3,17 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+function checkInt(num: number) {
+  if (!Number.isInteger(num)) {
+    throw new Error("Number is not an integer.");
+  }
+}
+
+try {
+  const num = 1;
+  checkInt(num);
+  console.log("Number is an integer.");
+} catch (error) {
+  console.log(error.message);
+}
