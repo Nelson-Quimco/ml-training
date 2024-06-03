ac0f2c5f975ae06013675b2e86d29acd43053b0a
Nelson <quimconelson@gmail.com>
2024-06-03 11:44:46 +0800

    [nodejs]continuation on making  a basic REST API that creates, updates, deletes, and retrieves a user.

--- week-1/nodejs/getMethods.js
+++ week-1/nodejs/getMethods.js
@@ -1,8 +1,9 @@
 const app = require("express")();
 const http = require("http");
+const fs = require("fs");
 const sampleData = require("../nodejs/data.json");
 const bodyParser = require("body-parser");
-const { log } = require("console");
+const { log, error } = require("console");
 const port = 3000;
 
 const server = http.createServer(app);
@@ -26,22 +27,59 @@ const getUserbyId = async (req, res) => {
 };
 
 const postUser = async (req, res) => {
-  sampleData.push(req.body);
-  return await res.json({ message: "Successfully aded!" });
+  const newUser = req.body;
+  sampleData.push(newUser);
+  try {
+    fs.writeFileSync("data.json", JSON.stringify(sampleData, null, 2));
+    return res.json({ message: "User Successfully Added", newUser });
+  } catch (error) {
+    console.log("Error", error);
+    return res.status(500).json({ message: "Error occured" });
+  }
 };
 
-const updateUser = async (req, res) => {};
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const user = sampleData.findIndex((user) => user.id === parseInt(id));
+  if (user === -1) {
+    return res.json({ message: "User Not Found" });
+  } else {
+    const updatedUser = {
+      ...sampleData[user],
+      ...req.body,
+    };
+
+    sampleData[user] = updatedUser;
+    const fs = require("fs");
+    fs.writeFile("data.json", JSON.stringify(sampleData), (err) => {
+      if (err) {
+        console.log(err);
+        return res.json({ message: "Error occurred while updating user" });
+      }
+      console.log("User updated:", updatedUser);
+      return res.json({ message: "Successfully Updated!" });
+    });
+  }
+};
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
-  const userData = sampleData.findIndex((user) => user.id === id);
+  const userData = sampleData.findIndex((user) => user.id === parseInt(id));
   console.log("makita;", userData);
   if (userData === -1) {
-    return await res.json({ message: "User Not Found" });
+    return res.json({ message: "User Not Found" });
   } else {
-    sampleData.splice(userData, 1)[0];
+    const deleteUser = sampleData.splice(userData, 1)[0];
+    fs.writeFile("data.json", JSON.stringify(sampleData), (err) => {
+      if (err) {
+        console.log("Error", err);
+        return res.json({ message: "Error occured while deleting the user" });
+      }
+      console.log("User deleted", deleteUser);
+      return res.json({ message: "Successfully Deleted!" });
+    });
   }
-  return await res.json({ message: "Successfully Deleted!" });
+  return;
 };
 
 app.get("/users", getUsers);
